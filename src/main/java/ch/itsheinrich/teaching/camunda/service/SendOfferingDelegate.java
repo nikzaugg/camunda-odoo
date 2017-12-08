e25dbe3220efe5a3574ae3e2f786fcf27b5ec3b5
Alex Scheitlin <alex.scheitlin@uzh.ch>
2017-12-08 17:30:46 +0100

    add send offering delegate class

---
+++ src/main/java/ch/itsheinrich/teaching/camunda/service/SendOfferingDelegate.java
@@ -0,0 +1,16 @@
+package ch.itsheinrich.teaching.camunda.service;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import javax.inject.Named;
+
+
+@Named("sendOfferingDelegate")
+public class SendOfferingDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegate) throws Exception {
+        System.out.println("SendOfferingDelegate");
+    }
+}
